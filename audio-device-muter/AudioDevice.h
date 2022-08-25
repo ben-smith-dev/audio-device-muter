0f7e9bcf36c6ac534cc05037448521559cf67665
Ben Smith <23388317+Bensmith9722@users.noreply.github.com>
2022-08-25 17:05:59 -0500

    Adds destructor declaration.

--- audio-device-muter/AudioDevice.h
+++ audio-device-muter/AudioDevice.h
@@ -19,6 +19,8 @@ public:
 		IPropertyStore* props
 	);
 
+	~AudioDevice();
+
 	HRESULT GetFriendlyName(LPWSTR* deviceFriendlyname);
 	HRESULT GetState(DWORD* state);
 	HRESULT GetMute(BOOL* isMuted);
