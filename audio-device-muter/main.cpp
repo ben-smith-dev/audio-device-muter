1449c3a24fa8dc3560ec4410b5408261b7cfd894
Ben Smith <23388317+Bensmith9722@users.noreply.github.com>
2022-08-25 17:07:46 -0500

    Adds getting default audio device and calling printDevices.
    - Currently debuging issues with device data pointers being null.

--- audio-device-muter/main.cpp
+++ audio-device-muter/main.cpp
@@ -1,6 +1,18 @@
-#include <iostream>
+#include "AudioDeviceManager.h"
+
+#include <stdio.h>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	HRESULT hr;
+
+	AudioDeviceManager* deviceManager = new AudioDeviceManager;
+
+	hr = deviceManager->GetDefaultAudioDevice();
+	if (FAILED(hr)) { return 1; }
+
+	hr = deviceManager->PrintDevices();
+	if (FAILED(hr)) { return 1; }
+
+	return 0;
 }
