10eb7259ce52698e53cd3ff1cf8a74b651f5f778
Ben Smith <23388317+Bensmith9722@users.noreply.github.com>
2022-08-25 14:36:25 -0500

    Adds method pointer variables.

--- audio-device-muter/AudioDevice.h
+++ audio-device-muter/AudioDevice.h
@@ -1,5 +1,13 @@
 #pragma once
 
+#include <windows.h>
+#include <mmdeviceapi.h>
+#include <endpointvolume.h>
+
 class AudioDevice
 {
+private:
+	IMMDevice* device;
+	IAudioEndpointVolume* endpointVolume;
+	IPropertyStore* props;
 };
