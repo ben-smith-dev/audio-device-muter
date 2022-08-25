5d77ad8365e55b51f4bef99b7ec14f701787d530
Ben Smith <23388317+Bensmith9722@users.noreply.github.com>
2022-08-25 14:57:04 -0500

    Adds declarations for class member methods.

--- audio-device-muter/AudioDevice.h
+++ audio-device-muter/AudioDevice.h
@@ -10,4 +10,19 @@ private:
 	IMMDevice* device;
 	IAudioEndpointVolume* endpointVolume;
 	IPropertyStore* props;
+
+public:
+	AudioDevice(
+		IMMDevice* device,
+		IAudioEndpointVolume* endpointVolume,
+		IPropertyStore* props
+	);
+
+	HRESULT GetFriendlyName(LPWSTR* deviceFriendlyname);
+	HRESULT GetState(DWORD* state);
+	HRESULT GetMute(BOOL* isMuted);
+	HRESULT SetMute(BOOL muteState);
+	HRESULT Print();
+
+	int ReleaseInterfaceReferences();
 };
