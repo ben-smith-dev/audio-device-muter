b44451436f3285a6a849dc962e08f49becdef0a9
Ben Smith <23388317+Bensmith9722@users.noreply.github.com>
2022-08-25 15:11:03 -0500

    Adds the class constructor and destructor definitions.

--- audio-device-muter/AudioDevice.cpp
+++ audio-device-muter/AudioDevice.cpp
@@ -1,1 +1,18 @@
 #include "AudioDevice.h"
+
+AudioDevice::AudioDevice(
+	IMMDevice* device,
+	IAudioEndpointVolume* endpointVolume,
+	IPropertyStore* props
+)
+{
+	this->device = device;
+	this->endpointVolume = endpointVolume;
+	this->props = props;
+}
+
+AudioDevice::~AudioDevice()
+{
+	// De-increments the refernce count of each interface.
+	AudioDevice::ReleaseInterfaceReferences();
+}
