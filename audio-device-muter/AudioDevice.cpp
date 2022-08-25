ed9a734dcbf4cdb4c2f7e8583f2dcc3fa027d3ef
Ben Smith <23388317+Bensmith9722@users.noreply.github.com>
2022-08-25 15:21:51 -0500

    Adds GetFriendlyName method definition.

--- audio-device-muter/AudioDevice.cpp
+++ audio-device-muter/AudioDevice.cpp
@@ -16,3 +16,24 @@ AudioDevice::~AudioDevice()
 	// De-increments the refernce count of each interface.
 	AudioDevice::ReleaseInterfaceReferences();
 }
+
+HRESULT AudioDevice::GetFriendlyName(LPWSTR* deviceFriendlyname)
+{
+	// Initialize the propvariant structure.
+	PROPVARIANT varName;
+	PropVariantInit(&varName);
+
+	//	Get device friendly name property
+	HRESULT hr = props->GetValue(
+		PKEY_Device_FriendlyName,
+		&varName
+	);
+	if (FAILED(hr)) { return hr; }
+
+	*deviceFriendlyname = varName.pwszVal;
+	
+	// Clear propvariant structure.
+	PropVariantClear(&varName);
+	
+	return S_OK;
+}
