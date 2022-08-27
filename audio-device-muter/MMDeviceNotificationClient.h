c24543ad3de4cc0233391ebdc07e4832ae586178
Ben Smith <23388317+Bensmith9722@users.noreply.github.com>
2022-08-27 18:28:49 -0500

    Adds class methods and variables.

--- audio-device-muter/MMDeviceNotificationClient.h
+++ audio-device-muter/MMDeviceNotificationClient.h
@@ -4,5 +4,41 @@
 
 class MMDeviceNotificationClient : public IMMNotificationClient
 {
+private:
+	LONG cRef;
+	EDataFlow dataFlow;
+	IMMDeviceEnumerator* deviceEnumerator;
 
+	HRESULT CheckDeviceDataFlow(EDataFlow* dataFlow);
+
+public:
+	MMDeviceNotificationClient();
+	~MMDeviceNotificationClient();
+
+	// IUnknown methods
+
+	ULONG STDMETHODCALLTYPE AddRef();
+	ULONG STDMETHODCALLTYPE Release();
+	HRESULT STDMETHODCALLTYPE QueryInterface();
+
+	// IMMNotificationClient callback methods
+
+	HRESULT STDMETHODCALLTYPE OnDeviceAdded(LPCWSTR pwstrDeviceId);
+	HRESULT STDMETHODCALLTYPE OnDeviceRemoved(LPCWSTR pwstrDeviceId);
+	
+	HRESULT STDMETHODCALLTYPE OnDefaultDeviceChanged(
+		EDataFlow flow, 
+		ERole role,
+		LPCWSTR deviceId
+	);
+	
+	HRESULT STDMETHODCALLTYPE OnDeviceStateChanged(
+		LPCWSTR deviceId,
+		DWORD newState
+	);
+	
+	HRESULT STDMETHODCALLTYPE OnPropertyValueChanged(
+		LPCWSTR deviceId,
+		const PROPERTYKEY propKey
+	);
 };
