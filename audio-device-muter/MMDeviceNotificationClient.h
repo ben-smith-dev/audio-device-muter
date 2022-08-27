d6f06101a1191af83c7bcf43c20c428c56e61366
Ben Smith <23388317+Bensmith9722@users.noreply.github.com>
2022-08-27 18:52:01 -0500

    Adds paramater to CheckDeviceDataFlow method.

--- audio-device-muter/MMDeviceNotificationClient.h
+++ audio-device-muter/MMDeviceNotificationClient.h
@@ -9,7 +9,7 @@ private:
 	EDataFlow dataFlow;
 	IMMDeviceEnumerator* deviceEnumerator;
 
-	HRESULT CheckDeviceDataFlow(EDataFlow* dataFlow);
+	HRESULT CheckDeviceDataFlow(LPCWSTR deviceId, EDataFlow* dataFlow);
 
 public:
 	MMDeviceNotificationClient(EDataFlow dataFlow);
