79264c8239f6f076d0fe2cf31afb17005e27bb16
Ben Smith <23388317+Bensmith9722@users.noreply.github.com>
2022-08-27 18:34:38 -0500

    Adds method definitions for IUnknown interface.

--- audio-device-muter/MMDeviceNotificationClient.cpp
+++ audio-device-muter/MMDeviceNotificationClient.cpp
@@ -1,1 +1,39 @@
 #include "MMDeviceNotificationClient.h"
+
+ULONG MMDeviceNotificationClient::AddRef()
+{
+	return InterlockedIncrement(&cRef);
+}
+
+ULONG MMDeviceNotificationClient::Release()
+{
+    ULONG ulRef = InterlockedDecrement(&cRef);
+    if (0 == ulRef)
+    {
+        delete this;
+    }
+    return ulRef;
+}
+
+HRESULT MMDeviceNotificationClient::QueryInterface(
+    REFIID riid, 
+    VOID** ppvInterface
+)
+{
+    if (IID_IUnknown == riid)
+    {
+        AddRef();
+        *ppvInterface = (IUnknown*)this;
+    }
+    else if (__uuidof(IMMNotificationClient) == riid)
+    {
+        AddRef();
+        *ppvInterface = (IMMNotificationClient*)this;
+    }
+    else
+    {
+        *ppvInterface = NULL;
+        return E_NOINTERFACE;
+    }
+    return S_OK;
+}
