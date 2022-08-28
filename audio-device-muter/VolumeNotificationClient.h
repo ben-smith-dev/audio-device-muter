0cdda433642dceab43699761e0db82ee40f45490
Ben Smith <23388317+Bensmith9722@users.noreply.github.com>
2022-08-28 01:03:34 -0500

    Adds member variable and method declarations.

--- audio-device-muter/VolumeNotificationClient.h
+++ audio-device-muter/VolumeNotificationClient.h
@@ -4,6 +4,25 @@
 
 class VolumeNotificationClient : public IAudioEndpointVolumeCallback
 {
+private:
+	LONG cRef;
+	
+	// Used to determine if a notification is needed. 
+	BOOL lastMuteState;
 
+public:
+
+	VolumeNotificationClient();
+	~VolumeNotificationClient();
+
+	// IUnkown methods
+
+	ULONG STDMETHODCALLTYPE AddRef();
+	ULONG STDMETHODCALLTYPE Release();
+	HRESULT STDMETHODCALLTYPE QueryInterface();
+
+	// IAudioEndpointVolumeCallback method
+
+	int OnNotify();
 };
 
