4a4a98942231cca80d9930cb2131f950c6ec2a0d
Ben Smith <23388317+Bensmith9722@users.noreply.github.com>
2022-08-27 23:21:55 -0500

    Removes calling RegisterForMMDeviceNotifications method.

--- audio-device-muter/main.cpp
+++ audio-device-muter/main.cpp
@@ -12,7 +12,7 @@ int main()
 	hr = CoInitializeEx(nullptr, tagCOINIT::COINIT_APARTMENTTHREADED);
 	if (FAILED(hr)) { return 1; }
 
-	hr = deviceManager->GetDefaultAudioDevice();
+	//hr = deviceManager->GetDefaultAudioDevice();
 	if (FAILED(hr)) { return 1; }
 
 	hr = deviceManager->GetDevices();
@@ -23,6 +23,16 @@ int main()
 
 	//LPCWSTR deviceID = (LPCWSTR)L"{0.0.1.00000000}.{a9c61a2a-bdcc-4f4b-9e98-fe919f6584c6}";
 	//hr = deviceManager->PrintDevice(&deviceID);
+	if (FAILED(hr)) { return 1; }
+
+	// Testing Notifaction callbacks.
+	while (true)
+	{
+		Sleep(10000);
+		printf("...\n");
+	}
+
+	hr = deviceManager->UnregisterFromMMDeviceNotifications();
 	if (FAILED(hr)) { return 1; }
 
 	CoUninitialize();
