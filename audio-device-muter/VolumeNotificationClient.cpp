9dc28aab96f6d81c53e25906ebdb1cdda5d09774
Ben Smith <23388317+Bensmith9722@users.noreply.github.com>
2022-08-28 01:12:29 -0500

    Adds IUnkown method definitions.
    - AddRef()
    - Release()
    - QueryInterface()

--- audio-device-muter/VolumeNotificationClient.cpp
+++ audio-device-muter/VolumeNotificationClient.cpp
@@ -5,3 +5,40 @@ VolumeNotificationClient::VolumeNotificationClient(BOOL muteState) : cRef(1)
 	this->lastMuteState = muteState;
 }
 
+ULONG VolumeNotificationClient::AddRef()
+{
+	return InterlockedIncrement(&cRef);
+}
+
+ULONG VolumeNotificationClient::Release()
+{
+	ULONG ulRef = InterlockedDecrement(&cRef);
+	if (0 == ulRef)
+	{
+		delete this;
+	}
+	return ulRef;
+}
+
+HRESULT VolumeNotificationClient::QueryInterface(
+	REFIID riid, 
+	VOID** ppvInterface
+)
+{
+	if (IID_IUnknown == riid)
+	{
+		AddRef();
+		*ppvInterface = (IUnknown*)this;
+	}
+	else if (__uuidof(IAudioEndpointVolumeCallback) == riid)
+	{
+		AddRef();
+		*ppvInterface = (IAudioEndpointVolumeCallback*)this;
+	}
+	else
+	{
+		*ppvInterface = NULL;
+		return E_NOINTERFACE;
+	}
+	return S_OK;
+}
